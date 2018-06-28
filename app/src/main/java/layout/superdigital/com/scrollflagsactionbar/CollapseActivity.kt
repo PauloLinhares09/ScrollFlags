508b84851a666b2490a82cfcae5176d508fdac7a
Paulo Linhares <plinhares9@gmail.com>
2018-06-28 16:33:58 -0300

    listener ok

--- app/src/main/java/layout/superdigital/com/scrollflagsactionbar/CollapseActivity.kt
+++ app/src/main/java/layout/superdigital/com/scrollflagsactionbar/CollapseActivity.kt
@@ -1,10 +1,15 @@
 package layout.superdigital.com.scrollflagsactionbar
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_collapse.*
+import kotlinx.android.synthetic.main.content_collapse.*
 
 class CollapseActivity : AppCompatActivity() {
 
@@ -15,6 +20,18 @@ class CollapseActivity : AppCompatActivity() {
 
         collapsing_toolbar_layout.title = "Ronaldo Fenômeno"
         collapsing_toolbar_layout.setExpandedTitleColor(resources.getColor(android.R.color.transparent))
+
+        //Listener para escutar mudanças de estado da AppBarLayout
+        app_bar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener{
+            override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+
+                if (verticalOffset == 0)
+                    tv_title_article.visibility = View.VISIBLE
+                else
+                    tv_title_article.visibility = View.INVISIBLE
+            }
+
+        })
 
 
         fab.setOnClickListener { view ->
