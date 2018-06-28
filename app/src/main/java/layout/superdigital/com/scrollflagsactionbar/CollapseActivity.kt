b389bc1b1d497da02d725ebb68dd7751fdd08027
Paulo Linhares <plinhares9@gmail.com>
2018-06-28 12:30:22 -0300

    title transp

--- app/src/main/java/layout/superdigital/com/scrollflagsactionbar/CollapseActivity.kt
+++ app/src/main/java/layout/superdigital/com/scrollflagsactionbar/CollapseActivity.kt
@@ -1,6 +1,7 @@
 package layout.superdigital.com.scrollflagsactionbar
 
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_collapse.*
@@ -11,6 +12,11 @@ class CollapseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collapse)
         setSupportActionBar(toolbar)
+
+        collapsing_toolbar_layout.title = "Ronaldo Fenômeno"
+        collapsing_toolbar_layout.setExpandedTitleColor(resources.getColor(android.R.color.transparent))
+
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
